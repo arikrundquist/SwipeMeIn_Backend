6e5807b4eb63f1b2227df1b77b9566ff5b477ab2
wongmac <hellogug@yahoo.com>
2018-10-20 16:49:04 -0500

    db

--- arik_handler.js
+++ arik_handler.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const AWS_DEPLOY_REGION = "us-east-1";
+const dynamoDb = new AWS.DynamoDB.DocumentClient({
+    api_version: '2012-08-10',
+    region: AWS_DEPLOY_REGION
+});
 
 module.exports.hello = async (event, context) => {
   return {
@@ -22,3 +27,50 @@ module.exports.hello = async (event, context) => {
    }),
   };
 };
+
+module.exports.dbTestCreate = async (event, context) => {
+  let _parsed;
+    try {
+      _parsed = JSON.parse(event.body);
+    } catch (err) {
+      console.error(`Could not parse requested JSON ${event.body}: ${err.stack}`);
+      return {
+        statusCode: 500,
+        error: `Could not parse requested JSON: ${err.stack}`
+      };
+    }
+    const { swipeID, swiperID, dateTime, swipeLimit, timeCreated, isDonation, swiperConfirmed, customerId, customerIdsConfirmed, transactionCompleted  } = _parsed;
+
+    const params = {
+      TableName: "Swipes",
+      Item: {
+        swipeId: swipeId,
+        swiperID: swiperID,
+        dateTime: dateTime,
+        swipeLimit: swipeLimit,
+        timeCreated: timeCreated,
+        isDonation: isDonation,
+        swiperConfirmed: swiperConfirmed,
+        customerId: customerId,
+        customerIdsConfirmed: customerIdsConfirmed,
+        transactionCompleted: transactionCompleted
+  },
+    };
+
+    return await new Promise((resolve, reject) => {
+      dynamoDb.put(params, (error, data) => {
+        if (error) {
+          console.log(`dbTestCreate ERROR=${error.stack}`);
+            resolve({
+              statusCode: 400,
+              error: `dbTestCreate: ${error.stack}`
+            });
+
+        } else {
+          console.log(`dbTestCreate data=${JSON.stringify(data)}`);
+          resolve({ statusCode: 200, body: JSON.stringify(params.Item) });
+        }
+      });
+    });
+
+  };
