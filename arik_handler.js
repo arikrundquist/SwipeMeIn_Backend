6eed90b2184c3c368bba011566127453a27dba6b
Arik Rundquist <arik.rundquist@gmail.com>
2018-10-20 15:21:29 -0500

    probably fixed...

--- arik_handler.js
+++ arik_handler.js
@@ -8,16 +8,15 @@ module.exports.hello = async (event, context) => {
       input: event,
     }),
   };
+};
+
 
   module.exports.thisIsATest = async(event, context) => {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'I wonder if this will work...',
-        input: event,
-      }),
-    };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+      message: 'I wonder if this will work...',
+      input: event,
+   }),
+  };
 };
