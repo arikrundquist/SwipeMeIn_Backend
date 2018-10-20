2ec156cb1f62c0637b468149dbd0c01b858580b3
Arik Rundquist <arik.rundquist@gmail.com>
2018-10-20 18:39:41 -0500

    postSwipe postDonation

--- arik_handler.js
+++ arik_handler.js
@@ -195,5 +195,5 @@ module.exports.getDonation = async (event, context) => {
   });
 */
 
-  return {thisWillProbablyNotWork: "but it might"}};
+  return {statusCode: 200, body: "but it might"}};
 };
