011dcb30aa937db79a5bf6014307da542044f8d4
Arik Rundquist <arik.rundquist@gmail.com>
2018-10-20 17:36:53 -0500

    formatting

--- arik_handler.js
+++ arik_handler.js
@@ -73,3 +73,97 @@ module.exports.dbTestCreate = async (event, context) => {
     });
   });
 };
+
+
+module.exports.postSwipe = async (event, context) => {
+  let _parsed;
+  try {
+    _parsed = JSON.parse(event.body);
+  } catch (err) {
+    console.error(`Could not parse requested JSON ${event.body}: ${err.stack}`);
+    return {
+      statusCode: 500,
+      error: `Could not parse requested JSON: ${err.stack}`
+    };
+  }
+  const { swipeId, swiperId, dateTime, swipeLimit, timeCreated, isDonation, swiperConfirmed, customerId, customerIdsConfirmed, transactionCompleted } = _parsed;
+
+  const params = {
+    TableName: "Swipes",
+    Item: {
+      swipeId: swipeId,
+      swiperId: swiperId,
+      dateTime: dateTime,
+      swipeLimit: swipeLimit,
+      timeCreated: timeCreated,
+      isDonation: isDonation,
+      swiperConfirmed: swiperConfirmed,
+      customerId: customerId,
+      customerIdsConfirmed: customerIdsConfirmed,
+      transactionCompleted: transactionCompleted
+    },
+  };
+
+  return await new Promise((resolve, reject) => {
+    dynamoDb.put(params, (error, data) => {
+      if (error) {
+        console.log(`dbTestCreate ERROR=${error.stack}`);
+        resolve({
+          statusCode: 400,
+          error: `dbTestCreate: ${error.stack}`
+        });
+
+      } else {
+        console.log(`dbTestCreate data=${JSON.stringify(data)}`);
+        resolve({ statusCode: 200, body: JSON.stringify(params.Item) });
+      }
+    });
+  });
+};
+
+
+module.exports.postDonation = async (event, context) => {
+  let _parsed;
+  try {
+    _parsed = JSON.parse(event.body);
+  } catch (err) {
+    console.error(`Could not parse requested JSON ${event.body}: ${err.stack}`);
+    return {
+      statusCode: 500,
+      error: `Could not parse requested JSON: ${err.stack}`
+    };
+  }
+  const { swipeId, swiperId, dateTime, swipeLimit, timeCreated, isDonation, swiperConfirmed, customerId, customerIdsConfirmed, transactionCompleted } = _parsed;
+
+  const params = {
+    TableName: "Donations",
+    Item: {
+      swipeId: swipeId,
+      swiperId: swiperId,
+      dateTime: dateTime,
+      swipeLimit: swipeLimit,
+      timeCreated: timeCreated,
+      isDonation: isDonation,
+      swiperConfirmed: swiperConfirmed,
+      customerId: customerId,
+      customerIdsConfirmed: customerIdsConfirmed,
+      transactionCompleted: transactionCompleted
+    },
+  };
+
+  return await new Promise((resolve, reject) => {
+    dynamoDb.put(params, (error, data) => {
+      if (error) {
+        console.log(`dbTestCreate ERROR=${error.stack}`);
+        resolve({
+          statusCode: 400,
+          error: `dbTestCreate: ${error.stack}`
+        });
+
+      } else {
+        console.log(`dbTestCreate data=${JSON.stringify(data)}`);
+        resolve({ statusCode: 200, body: JSON.stringify(params.Item) });
+      }
+    });
+  });
+};
