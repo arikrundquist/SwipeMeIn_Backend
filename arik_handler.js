e1d9d50d75457ce19235edc885cb187da402dc74
Arik Rundquist <arik.rundquist@gmail.com>
2018-10-20 15:04:01 -0500

    another js error

--- arik_handler.js
+++ arik_handler.js
@@ -9,6 +9,15 @@ module.exports.hello = async (event, context) => {
     }),
   };
 
+  module.exports.thisIsATest = async(event, context, var1, var2) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'I wonder if this will work... var1 = ' + var1 + ' and var2 = ' + var2,
+        input: event,
+      }),
+    };
+
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
