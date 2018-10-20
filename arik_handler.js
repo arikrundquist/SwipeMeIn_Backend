40e305d4190cbb38977be2d9f517c05c8fde3341
Arik Rundquist <arik.rundquist@gmail.com>
2018-10-20 17:28:42 -0500

    formatting

--- arik_handler.js
+++ arik_handler.js
@@ -18,10 +18,10 @@ module.exports.hello = async (event, context) => {
 };
 
 
-  module.exports.thisIsATest = async(event, context) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
+module.exports.thisIsATest = async(event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
       message: 'I wonder if this will work...',
       input: event,
    }),
@@ -30,47 +30,46 @@ module.exports.hello = async (event, context) => {
 
 module.exports.dbTestCreate = async (event, context) => {
   let _parsed;
-    try {
-      _parsed = JSON.parse(event.body);
-    } catch (err) {
-      console.error(`Could not parse requested JSON ${event.body}: ${err.stack}`);
-      return {
-        statusCode: 500,
-        error: `Could not parse requested JSON: ${err.stack}`
-      };
-    }
-    const { swipeId, swiperId, dateTime, swipeLimit, timeCreated, isDonation, swiperConfirmed, customerId, customerIdsConfirmed, transactionCompleted  } = _parsed;
-
-    const params = {
-      TableName: "Swipes",
-      Item: {
-        swipeId: swipeId,
-        swiperId: swiperId,
-        dateTime: dateTime,
-        swipeLimit: swipeLimit,
-        timeCreated: timeCreated,
-        isDonation: isDonation,
-        swiperConfirmed: swiperConfirmed,
-        customerId: customerId,
-        customerIdsConfirmed: customerIdsConfirmed,
-        transactionCompleted: transactionCompleted
-  },
+  try {
+    _parsed = JSON.parse(event.body);
+  } catch (err) {
+    console.error(`Could not parse requested JSON ${event.body}: ${err.stack}`);
+    return {
+      statusCode: 500,
+      error: `Could not parse requested JSON: ${err.stack}`
     };
+  }
+  const { swipeId, swiperId, dateTime, swipeLimit, timeCreated, isDonation, swiperConfirmed, customerId, customerIdsConfirmed, transactionCompleted } = _parsed;
 
-    return await new Promise((resolve, reject) => {
-      dynamoDb.put(params, (error, data) => {
-        if (error) {
-          console.log(`dbTestCreate ERROR=${error.stack}`);
-            resolve({
-              statusCode: 400,
-              error: `dbTestCreate: ${error.stack}`
-            });
-
-        } else {
-          console.log(`dbTestCreate data=${JSON.stringify(data)}`);
-          resolve({ statusCode: 200, body: JSON.stringify(params.Item) });
-        }
-      });
-    });
-
+  const params = {
+    TableName: "Swipes",
+    Item: {
+      swipeId: swipeId,
+      swiperId: swiperId,
+      dateTime: dateTime,
+      swipeLimit: swipeLimit,
+      timeCreated: timeCreated,
+      isDonation: isDonation,
+      swiperConfirmed: swiperConfirmed,
+      customerId: customerId,
+      customerIdsConfirmed: customerIdsConfirmed,
+      transactionCompleted: transactionCompleted
+    },
   };
+
+  return await new Promise((resolve, reject) => {
+    dynamoDb.put(params, (error, data) => {
+      if (error) {
+        console.log(`dbTestCreate ERROR=${error.stack}`);
+        resolve({
+          statusCode: 400,
+          error: `dbTestCreate: ${error.stack}`
+        });
+
+      } else {
+        console.log(`dbTestCreate data=${JSON.stringify(data)}`);
+        resolve({ statusCode: 200, body: JSON.stringify(params.Item) });
+      }
+    });
+  });
+};
